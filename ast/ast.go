83123093068566e672824c127bb672a23d1d34f2
achmad <faa04807@gmail.com>
2024-08-24 19:45:38 +0700

    chore(ast): add some method

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,10 @@
 package ast
 
-import "bytes"
+import (
+	"bytes"
+
+	"github.com/achmad-dev/leah/token"
+)
 
 // base nodei interface
 type NodeI interface {
@@ -20,8 +24,32 @@ type ExpressionI interface {
 	expressionNode()
 }
 
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 type Program struct {
 	Statements []StatementI
+}
+
+func (p *Program) TokenLiteral() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.TokenLiteral())
+		out.WriteString("\n")
+	}
+	return out.String()
 }
 
 func (p *Program) String() string {
@@ -30,5 +58,27 @@ func (p *Program) String() string {
 		out.WriteString(s.String())
 		out.WriteString("\n")
 	}
+	return out.String()
+}
+
+type VarStatement struct {
+	Value ExpressionI
+	Name  *Identifier
+	Token token.Token
+}
+
+func (v *VarStatement) TokenLiteral() string {
+	return v.Token.Literal
+}
+
+func (v *VarStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(v.TokenLiteral() + " ")
+	out.WriteString(v.Name.String())
+	out.WriteString(" = ")
+	if v.Value != nil {
+		out.WriteString(v.Value.String())
+	}
+	out.WriteString(";")
 	return out.String()
 }
